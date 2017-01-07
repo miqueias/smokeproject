aca8cac3cabb5e171e588b40758644d648800c6f
Miqueias Lopes <miqueias.lopes@gmail.com>
2017-01-07 17:12:23 -0300

    Criação do enum Method; Criação do pacote Request; Criação do classe BaseRequest;

---
+++ app/src/main/java/Request/BaseRequest.java
@@ -0,0 +1,200 @@
+package Request;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by Miqueias on 1/7/17.
+ */
+
+public class BaseRequest extends AsyncTask<BaseRequest, Object, String> {
+
+    private String url;
+    private JSONObject jsonObject;
+    private Method method;
+    private Context context;
+    private String strReturn;
+    private String gsonString;
+    private String authorization;
+
+    BaseRequest() {
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public JSONObject getJsonObject() {
+        return jsonObject;
+    }
+
+    public void setJsonObject(JSONObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public String getStrReturn() {
+        return strReturn;
+    }
+
+    public void setStrReturn(String strReturn) {
+        this.strReturn = strReturn;
+    }
+
+    public String getGsonString() {
+        return gsonString;
+    }
+
+    public void setGsonString(String gsonString) {
+        this.gsonString = gsonString;
+    }
+
+    public String getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(String authorization) {
+        this.authorization = authorization;
+    }
+
+    @Override
+    protected String doInBackground(BaseRequest... params) {
+        try {
+            return sendGson(this.url, this.method, this.gsonString, this.authorization);//sendPostRequest(this.url, this.method, this.jsonObject);
+        } catch (JSONException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static String sendGson(String apiUrl, Method method, String gsonString, String authorization) throws JSONException, IOException {
+
+        URL url;
+        String returnStr = "";
+
+        try {
+            url = new URL(apiUrl);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("invalid url: " + apiUrl);
+        }
+
+        try {
+            if (method == Method.POST) {
+
+                HttpsURLConnection conn = null;
+                byte[] bytes = null;
+                conn = (HttpsURLConnection) url.openConnection();
+
+                String body = "";
+                if (gsonString != null) {
+                    body = gsonString;
+                }
+
+                bytes = body.getBytes();
+                conn.setRequestProperty("Content-Type", "application/json");
+                if (authorization != null && authorization.trim() != "") {
+                    conn.setRequestProperty("Authorization", "JWT " + authorization);
+                }
+
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod(String.valueOf(method));
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream out = conn.getOutputStream();
+                out.write(bytes);
+                out.close();
+
+                int status = conn.getResponseCode();
+
+                InputStream is;
+                String convertStreamToString = "";
+
+                if (status == 400 || status == 500) {
+                    //return MessageText.ERROR_SERVER.toString();
+                    convertStreamToString = convertStreamToString(conn.getErrorStream(), "UTF-8");
+                } else {
+                    if (conn != null) {
+                        convertStreamToString = convertStreamToString(conn.getInputStream(), /*HTTP.UTF_8*/"UTF-8");
+                        conn.disconnect();
+                    }
+                }
+                //returnStr = convertStreamToString;
+                return convertStreamToString;
+
+            }
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+        return returnStr;
+    }
+
+
+    private static String convertStreamToString(InputStream is, String enc) throws UnsupportedEncodingException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, enc));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        try
+        {
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                is.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ app/src/main/java/util/Util.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Miqueias on 1/7/17.
+ */
+
+public class Util {
+
+    Util() {
+
+    }
+
+    public static String ChangeStringTosha1(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+}
