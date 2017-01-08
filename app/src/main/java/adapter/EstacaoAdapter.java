05d88334229c527f98a51d968c6b37295cb109b6
Miqueias Lopes <miqueias.lopes@gmail.com>
2017-01-08 17:45:15 -0300

    criação do adapter estação; carregar o adapter na tela de lista de estação;

---
+++ app/src/main/java/adapter/EstacaoAdapter.java
@@ -0,0 +1,78 @@
+package adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import br.com.monster.smokeproject.R;
+import pojo.EstacoesElevatorias;
+
+/**
+ * Created by Miqueias on 1/8/17.
+ */
+
+public class EstacaoAdapter extends RecyclerView.Adapter<EstacaoAdapter.PersonViewHolder> {
+
+    private ArrayList<EstacoesElevatorias> lista;
+    public Context context;
+
+    public EstacaoAdapter() {
+
+    }
+
+    public EstacaoAdapter(Context context, ArrayList<EstacoesElevatorias> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
+
+    public EstacaoAdapter(ArrayList<EstacoesElevatorias> lista) {
+        this.lista = lista;
+    }
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+
+        Typeface RalewayMedium = Typeface.createFromAsset(itemView.getResources().getAssets(), "Raleway-Medium.ttf");
+        TextView tvSupervisor;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            itemView.setClickable(true);
+            tvSupervisor = (TextView) itemView.findViewById(R.id.tvNomeEstacao);
+            tvSupervisor.setTypeface(RalewayMedium);
+        }
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_nova_vistoria, viewGroup, false);
+        EstacaoAdapter.PersonViewHolder pvh = new EstacaoAdapter.PersonViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(final PersonViewHolder personViewHolder, int position) {
+        personViewHolder.tvSupervisor.setText(lista.get(position).getDescricao());
+//        personViewHolder.tvNomeEstacao.setText(endereco.get(position).getComplemento());
+//        personViewHolder.imgCheck.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                personViewHolder.imgCheck.setVisibility(View.INVISIBLE);
+//            }
+//        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+}
